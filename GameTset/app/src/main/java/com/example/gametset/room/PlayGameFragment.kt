5add2b0d7a3b15b9c4a017c4136a3ce4c26e29aa
DESKTOP-4QCTMKG\SSAFY <tjddnjs6331@naver.com>
2025-02-03 10:58:52 +0900

    fix: playfragment 수정중
    Former-commit-id: 53ee8a4cf6f6fd6c669426058fd323d623c3c81f

--- GameTset/app/src/main/java/com/example/gametset/room/PlayGameFragment.kt
+++ GameTset/app/src/main/java/com/example/gametset/room/PlayGameFragment.kt
@@ -1,60 +1,220 @@
 package com.example.gametset.room
 
+import android.graphics.Path
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.SeekBar
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.gametset.R
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.WebSocket
+import okhttp3.WebSocketListener
+import org.json.JSONObject
+import com.example.gametset.databinding.FragmentPlayGameBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val TAG = "PlayGameFragment_싸피"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PlayGameFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PlayGameFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    // 뷰 바인딩 객체
+    private var _binding: FragmentPlayGameBinding? = null
+    private val binding get() = _binding!!
+
+    // 시뮬레이션된 경로 목록
+    private lateinit var simulatedPaths: List<Path>
+
+    // WebSocket 객체
+    private lateinit var webSocket: WebSocket
+
+    // 어댑터 객체
+    private lateinit var wordAdapter: WordAdapter
+
+    // 단어 목록
+    private val wordList = mutableListOf<String>()
+
+    // 단어 목록
+    private var currentColor = 0
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentPlayGameBinding.bind(view)
+
+        wordAdapter = WordAdapter(wordList)
+        binding.wordRecyclerView.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        binding.wordRecyclerView.adapter = wordAdapter
+        currentColor = ContextCompat.getColor(requireContext(), R.color.black)
+
+        // WebSocket 연결 초기화
+        setupWebSocketConnection()
+
+        // DrawingView에 WebSocket 전달
+        binding.drawingView.setWebSocket(webSocket)
+
+        // 펜,지우개 모드
+        binding.apply {
+            pen.setOnClickListener {
+                binding.drawingView.drawPaint.color = currentColor
+                binding.drawingView.eraserEnabled = false
+            }
+            eraser.setOnClickListener {
+                binding.drawingView.drawPaint.color =
+                    ContextCompat.getColor(requireContext(), R.color.white)
+                sendColorChangeMessage("#FFFFFF")
+                binding.drawingView.eraserEnabled = true
+            }
+        }
+
+        // Start 버튼 클릭 시 메시지 전송
+        binding.startButton.setOnClickListener {
+            val text = binding.answer.text.toString()
+            if (text.isEmpty()) {
+                Toast.makeText(requireContext(), "입력창이 비었습니다!", Toast.LENGTH_SHORT).show()
+            } else {
+                sendMessage(text)
+                wordAdapter.addWord(text, binding.wordRecyclerView)
+            }
+        }
+
+        // Clear 버튼 클릭 시 그림판 지우기
+        binding.clearButton.setOnClickListener {
+            binding.drawingView.sendClearDrawing()
+        }
+
+        // 색상 선택 버튼 클릭 이벤트 처리
+        binding.apply {
+            setupColorButton(penColorRed, R.color.red, "#FF0000")
+            setupColorButton(penColorOrange, R.color.orange, "#FF7F50")
+            setupColorButton(penColorYellow, R.color.yellow, "#FFFF00")
+            setupColorButton(penColorGreen, R.color.green, "#ADFF2F")
+            setupColorButton(penColorBlue, R.color.blue, "#0000FF")
+            setupColorButton(penColorNavyBlue, R.color.navy_blue, "#000080")
+            setupColorButton(penColorPurple, R.color.purple, "#800080")
+            setupColorButton(penColorBlack, R.color.black, "#FF000000")
+        }
+
+        binding.verticalSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                setPencilSize(progress.toFloat()) // SeekBar의 현재 값을 연필 크기로 설정
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // 사용자가 SeekBar를 터치하기 시작할 때 호출됩니다.
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // 사용자가 SeekBar를 터치한 후 손을 뗄 때 호출됩니다.
+            }
+        })
+    }
+
+    // 연필 색 바꾸는 함수
+    private fun setupColorButton(imageView: ImageView, colorResId: Int, colorHex: String) {
+        imageView.setOnClickListener {
+            binding.apply {
+                // 펜 모드 일때
+                if (!binding.drawingView.eraserEnabled) {
+                    currentColor = ContextCompat.getColor(requireContext(), colorResId)
+                    drawingView.drawPaint.color = currentColor
+                    sendColorChangeMessage(colorHex)
+                }
+            }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_play_game, container, false)
+    //연필 크기 바꾸는 함수
+    private fun setPencilSize(size: Float) {
+        binding.drawingView.drawPaint.strokeWidth = size
+        binding.drawingView.erasePaint.strokeWidth = size
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PlayGameFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PlayGameFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    // WebSocket 연결 설정
+    private fun setupWebSocketConnection() {
+//        val serverUrl = "ws://192.168.100.203:9987/ws"
+        val serverUrl = "wss://i12d108.p.ssafy.io/api/ws"
+        val client = OkHttpClient()
+        val request = Request.Builder().url(serverUrl).build()
+
+        // WebSocket 리스너 설정
+        webSocket = client.newWebSocket(request, object : WebSocketListener() {
+            override fun onMessage(webSocket: WebSocket, text: String) {
+                requireActivity().runOnUiThread {
+                    try {
+                        val json = JSONObject(text)
+                        val event = json.optString("event", "")
+
+                        when (event) {
+                            "draw" -> {
+                                val x = json.optDouble("x", -1.0).toFloat()
+                                val y = json.optDouble("y", -1.0).toFloat()
+                                val color = json.optString("color", "#000000")
+                                val mode = json.optInt("mode", 1)
+                                if (x >= 0 && y >= 0) {
+                                    binding.drawingView.drawFromServer(x, y, color, mode)
+                                }
+                            }
+
+                            "colorChange" -> {
+                                val color = json.optString("color", "#000000")
+                                binding.drawingView.updateColorFromServer(color)
+                            }
+
+                            "clearDrawing" -> binding.drawingView.clearDrawing()
+                            else -> wordAdapter.addWord(text, binding.wordRecyclerView)
+                        }
+
+                    } catch (e: Exception) {
+                        Log.e(TAG, "🚨 WebSocket 메시지 파싱 오류: ${e.message}")
+                    }
                 }
             }
+
+            override fun onFailure(
+                webSocket: WebSocket,
+                t: Throwable,
+                response: okhttp3.Response?
+            ) {
+                super.onFailure(webSocket, t, response)
+                requireActivity().runOnUiThread {
+                    Log.d(TAG, "onFailure: ${t.message}")
+                    Log.d(TAG, "Response: ${response?.code} and ${response?.isSuccessful}")
+
+                    Toast.makeText(requireContext(), "Error: ${t.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onOpen(webSocket: WebSocket, response: okhttp3.Response) {
+                super.onOpen(webSocket, response)
+                sendMessage("Hello from Android!")
+            }
+        })
+    }
+
+    // 메시지 전송
+    private fun sendMessage(message: String) {
+        webSocket.send(message)
+    }
+
+    // Fragment에서 색상 변경 시 호출
+    private fun sendColorChangeMessage(color: String) {
+        val json = JSONObject().apply {
+            put("event", "colorChange")
+            put("color", color)
+            put("roomId", "roomId123")
+        }
+        webSocket.send(json.toString())
+    }
+
+    // Fragment 종료 시 WebSocket 연결 종료
+    override fun onDestroyView() {
+        super.onDestroyView()
+        webSocket.close(1000, "App closed")
+        _binding = null
     }
 }
