dd76ae52e9a6a64add2120b955158f76705e086a
도승국 <dororo737@gmail.com>
2025-02-04 18:17:22 +0900

    fix: 도현이 오류 수정
    Former-commit-id: ed9bdb43ca581e08adbf43f7038602bfca658e93

--- D108/src/main/java/com/example/model/service/UserServiceImpl.java
+++ D108/src/main/java/com/example/model/service/UserServiceImpl.java
@@ -1,6 +1,6 @@
 package com.example.model.service;
 
-import com.example.controller.FriendRestController;
+import com.example.controller.FriendController;
 import com.example.model.dao.UserDao;
 import com.example.model.dto.User;
 import org.slf4j.Logger;
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl implements UserService {
 
-    private static final Logger logger = LoggerFactory.getLogger(FriendRestController.class);
+    private static final Logger logger = LoggerFactory.getLogger(FriendController.class);
 
     private final UserDao userDao;
 
