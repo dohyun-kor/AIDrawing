1b94403ffa945641bb76964e15998f9504810f9e
도승국 <dororo737@gmail.com>
2025-02-10 18:47:26 +0900

    fix env
    Former-commit-id: 46fa65ff6c559d0d51538a3f07edbc387ec075c3

--- D108/src/main/java/com/example/Application.java
+++ D108/src/main/java/com/example/Application.java
@@ -12,11 +12,6 @@ import org.springframework.cache.annotation.EnableCaching;
 @MapperScan(basePackageClasses = UserDao.class)
 public class Application {
     public static void main(String[] args) {
-        // .env 파일 로드
-        Dotenv dotenv = Dotenv.load();
-        // .env 파일에서 값을 읽어 시스템 속성에 설정 (환경 변수로 사용)
-        System.setProperty("openai.api.key", dotenv.get("OPENAI_API_KEY"));
-
         SpringApplication.run(Application.class, args);
     }
 }
