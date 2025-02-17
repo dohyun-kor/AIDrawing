dce91af55db2f8cce2bd0de13037d813fb743537
도승국 <dororo737@gmail.com>
2025-02-17 15:27:50 +0900

    fix: change category
    Former-commit-id: 3285cdbf145efcf989abb143cf2b1936ec021fe3

--- D108/src/main/java/com/example/model/dto/PurchaseRequestDto.java
+++ D108/src/main/java/com/example/model/dto/PurchaseRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Setter
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class PurchaseRequestDto {
 // Getter/Setter
     /**
@@ -16,12 +18,6 @@ public class PurchaseRequestDto {
     private int userId;      // 구매자 사용자 ID
     private int itemId;      // 구매할 아이템 ID
     private int itemPrice;   // 아이템 가격
-
-    // 모든 필드를 받는 생성자
-    public PurchaseRequestDto(int userId, int itemId, int itemPrice) {
-        this.userId = userId;
-        this.itemId = itemId;
-        this.itemPrice = itemPrice;
-    }
+    private String category;
 
 }
